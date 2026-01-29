7c3b39a2aa014eb275ef53c6a20c99516c4ed957
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-01-29 23:38:25 +0700

    feat: adding app and rest framework in settings.py

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_app.apps.DrfAppConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
